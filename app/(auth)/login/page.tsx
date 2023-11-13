1e4bccac89ca009fd8d53bde465832136a8597c7
Dzenis Gudzevic <frontprogramer@gmail.com>
2023-11-13 12:47:19 +0100

    Added changes code

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -1,21 +1,21 @@
-'use client'
-import LoginCard from '@/components/auth/login/LoginCard'
-import { useSession } from 'next-auth/react'
-import { useRouter } from 'next/navigation'
+"use client";
+import LoginCard from "@/components/auth/login/LoginCard";
+import { useSession } from "next-auth/react";
+import { useRouter } from "next/navigation";
 
 const LoginPage = () => {
-  const { data: session } = useSession()
-  const router = useRouter()
+  const { data: session } = useSession();
+  const router = useRouter();
 
   if (session?.user) {
-    router.push('/')
+    router.push("/");
   }
 
   return (
     <section className="flex justify-center items-center p-3 h-screen">
       <LoginCard />
     </section>
-  )
-}
+  );
+};
 
-export default LoginPage
+export default LoginPage;
